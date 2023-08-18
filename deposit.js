a4ef9ecffd353a677efcfb34a25c473415790ba3
sami5671 <samialam5671@gmail.com>
2023-08-18 16:13:38 +0600

    update

--- deposit.js
+++ deposit.js
@@ -1,3 +1,15 @@
+/**
+ * 1. add event listener to the deposit button
+ * 2. get deposit amount from input field
+ * 2.5. convert string deposit amount to a number type
+ *
+ * 3. clear the deposit input field after getting the value
+ * 4. get the previous deposit total
+ * 5. calculate new deposit total and set the value to the deposit total
+ * 6. get current balance total
+ * 7. calculate the new balance and set it to the balance total element
+ */
+
 // step 1: ADD event listener to the deposit button
 document.getElementById("btn-deposit").addEventListener("click", function () {
   //step 2: get the deposit amount from the deposit input field
