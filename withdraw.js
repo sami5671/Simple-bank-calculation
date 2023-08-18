4de51106925146cbc7b48ef45af1a1f402c6527e
sami5671 <samialam5671@gmail.com>
2023-08-18 08:40:15 +0600

    update

--- withdraw.js
+++ withdraw.js
@@ -17,24 +17,30 @@ document.getElementById("btn-withdraw").addEventListener("click", function () {
   const withdrawField = document.getElementById("withdraw-field");
   const newWithdrawAmountString = withdrawField.value;
   const newWithdrawAmount = parseFloat(newWithdrawAmountString);
-
+  // step 7:
+  withdrawField.value = "";
+  if (isNaN(newWithdrawAmount)) {
+    alert("please provide a valid number!!!");
+    return;
+  }
   //   step 3:
   const withdrawTotalElement = document.getElementById("withdraw-total");
   const previousWithdrawTotalString = withdrawTotalElement.innerText;
   const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
-
-  //   step 4:
-  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  withdrawTotalElement.innerText = currentWithdrawTotal;
 
   //   step 5:
   const balanceTotalElement = document.getElementById("balance-total");
   const previousBalanceTotalString = balanceTotalElement.innerText;
   const previousBalanceTotal = parseFloat(previousBalanceTotalString);
 
+  if (newWithdrawAmount > previousBalanceTotal) {
+    alert("ato tk nai!!!");
+    return;
+  }
+  //   step 4:
+  const currentWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+  withdrawTotalElement.innerText = currentWithdrawTotal;
   //   step 6:
   const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
   balanceTotalElement.innerText = newBalanceTotal;
-  // step 7:
-  withdrawField.value = "";
 });
